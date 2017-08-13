4ea24072e112113f52c5b0e102a30a9052fe63f7
Iqbal <iqbalchoudhury2@gmail.com>
2017-08-12 23:30:03 -0500

    passing Parameter from feature file

--- Features/SignUp.feature
+++ Features/SignUp.feature
@@ -2,15 +2,15 @@ Feature: SignUpFeature
 		This feature deals with sign up functionality of the application
 	
 	Scenario: Sign up with valid user data
-	Given user navigage to homme page
+	Given user navigage to hommepage
 	And  User click my account
 	And User Click sign up
-	And User enter first name
-	And User enter last name
-	And User enter mobile number
-	And User enter email 
-	And User enter password
-	And User confirms password
+	And User enter first name as "Choudhury"
+	And User enter lastName as "Iqbal"
+	And User enter mobile number as "9293856129"
+	And User enter email as "iqbalchoudhury2@gmail.com"
+	And User enter password as "password"
+	And User confirms password as "password"
 	When User click sign up button
 	Then User will be able to sign up successfully
 	 
--- src/test/java/stepDefinations/SignUp.java
+++ src/test/java/stepDefinations/SignUp.java
@@ -11,59 +11,65 @@ import cucumber.api.java.en.*;
  *
  */
 public class SignUp {
-	@Given("^user navigage to homme page$")
-	public void user_navigage_to_homme_page() throws Throwable {
 
-	}
+@Given("^user navigage to hommepage$")
+public void user_navigage_to_hommepage() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+@Given("^User click my account$")
+public void user_click_my_account() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+@Given("^User enter first name as \"([^\"]*)\"$")
+public void user_enter_first_name_as(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
 
-	@Given("^User click my account$")
-	public void user_click_my_account() throws Throwable {
+@Given("^User enter lastName as \"([^\"]*)\"$")
+public void user_enter_lastName_as(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
 
-	}
+@Given("^User enter mobile number as \"([^\"]*)\"$")
+public void user_enter_mobile_number_as(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
 
-	@Given("^User Click sign up$")
-	public void user_Click_sign_up() throws Throwable {
+@Given("^User enter email as \"([^\"]*)\"$")
+public void user_enter_email_as(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    System.out.println("your email is "+arg1);
+}
 
-	}
+@Given("^User enter password as \"([^\"]*)\"$")
+public void user_enter_password_as(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
 
-	@Given("^User enter first name$")
-	public void user_enter_first_name() throws Throwable {
+@Given("^User confirms password as \"([^\"]*)\"$")
+public void user_confirms_password_as(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+@Given("^User Click sign up$")
+public void user_Click_sign_up() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+@When("^User click sign up button$")
+public void user_click_sign_up_button() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
 
-	}
-
-	@Given("^User enter last name$")
-	public void user_enter_last_name() throws Throwable {
-
-	}
-
-	@Given("^User enter mobile number$")
-	public void user_enter_mobile_number() throws Throwable {
-
-	}
-
-	@Given("^User enter email$")
-	public void user_enter_email() throws Throwable {
-
-	}
-
-	@Given("^User enter password$")
-	public void user_enter_password() throws Throwable {
-
-	}
-
-	@Given("^User confirms password$")
-	public void user_confirms_password() throws Throwable {
-
-	}
-
-	@When("^User click sign up button$")
-	public void user_click_sign_up_button() throws Throwable {
-
-	}
-
-	@Then("^User will be able to sign up successfully$")
-	public void user_will_be_able_to_sign_up_successfully() throws Throwable {
-
-	}
-
+}
+@Then("^User will be able to sign up successfully$")
+public void user_will_be_able_to_sign_up_successfully() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
 }
